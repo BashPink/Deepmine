7e9cb2482e828fea0a7129e7db3db9839d2c2440
Aaron Kincer <94083098+BashPink@users.noreply.github.com>
2021-12-07 17:32:20 -0500

    Update Vminer.sh

--- Vminer.sh
+++ Vminer.sh
@@ -1,15 +1,15 @@
 #!/bin/bash
 
 #Call config.txt as a variable to use in executing command
-wal='cat config.txt'
+wal="home/pi/Deepmine/config.txt" 
 
 #User input 
 echo "Please select an algorythm. [ x17 / scrypt ]"
 read -r algo
      if [ "$algo" == x17 ]; then
-     sudo /home/pi/cpuminer-multi/cpuminer -a x17 -o stratum+tcp://xvg-x17.f2pool.com:9010 -u "$wal"
+     sudo /home/pi/cpuminer-multi/cpuminer -a x17 -o stratum+tcp://xvg-x17.f2pool.com:9010 -u $wal
      fi
 
      if [ "$algo" == scrypt ]; then
-     sudo /home/pi/cpuminer-multi/cpuminer -a scrypt -o stratum+tcp://xvg-scrypt.f2pool.com:9010 -u "$wal"
+     sudo /home/pi/cpuminer-multi/cpuminer -a scrypt -o stratum+tcp://xvg-scrypt.f2pool.com:9010 -u $wal
      fi
