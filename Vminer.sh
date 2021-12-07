416ce3da34ff947d3338acaeebdbf07a69f4d51e
Aaron Kincer <94083098+BashPink@users.noreply.github.com>
2021-12-07 17:38:53 -0500

    Update Vminer.sh

--- Vminer.sh
+++ Vminer.sh
@@ -7,9 +7,9 @@ wal="home/pi/Deepmine/config.txt"
 echo "Please select an algorythm. [ x17 / scrypt ]"
 read -r algo
      if [ "$algo" == x17 ]; then
-     sudo /home/pi/cpuminer-multi/cpuminer -a x17 -o stratum+tcp://xvg-x17.f2pool.com:9010 -u $wal
+     sudo /home/pi/cpuminer-multi/cpuminer -a x17 -o stratum+tcp://xvg-x17.f2pool.com:9010 -u "$wal"
      fi
 
      if [ "$algo" == scrypt ]; then
-     sudo /home/pi/cpuminer-multi/cpuminer -a scrypt -o stratum+tcp://xvg-scrypt.f2pool.com:9010 -u $wal
+     sudo /home/pi/cpuminer-multi/cpuminer -a scrypt -o stratum+tcp://xvg-scrypt.f2pool.com:9010 -u "$wal"
      fi
